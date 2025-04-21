a3b05aa4ab711a5d601bf03fc5b537a2e1a53a47
AutoDeployBot <autobot@example.com>
2025-04-21 10:16:54 +0530

    Auto update proxy URL

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 # Set your public proxy URL here (LocalTunnel)
-PROXY_BASE_URL = "https://social-towns-fall.loca.lt"
+PROXY_BASE_URL = "https://common-bikes-relax.loca.lt"
 
 @app.route("/besttrade")
 def best_trade():
